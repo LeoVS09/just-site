698478a867ef5b2d5f2a61d27289993227e91729
LeoVS09 <LeoVS010@gmail.com>
2017-02-24 22:42:05 +0300

    add paralax

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin({template: './src/index.jade', inject: "head"}),
+        new HtmlWebpackPlugin({template: './src/index.jade'}), //, inject: "head"
         new webpack.LoaderOptionsPlugin({
             options: {
                 stylus: {
